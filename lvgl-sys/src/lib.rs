35dbaaf3e84540ba8cb63bbc00de551e1eca4812
Rafael Caricio <rafael@caricio.com>
2020-04-10 20:53:41 +0200

    Basic sanity check test

--- lvgl-sys/src/lib.rs
+++ lvgl-sys/src/lib.rs
@@ -3,3 +3,21 @@
 #![allow(non_upper_case_globals)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn basic_sanity_check() {
+        unsafe {
+            lv_init();
+
+            let hres = lv_disp_get_hor_res(std::ptr::null_mut());
+            assert_eq!(hres, 480);
+
+            let vres = lv_disp_get_ver_res(std::ptr::null_mut());
+            assert_eq!(vres, 320);
+        }
+    }
+}
