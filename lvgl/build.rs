43f2a2d4c404bd4253baa8e9dc21008cfc6b8f4b
Rafael Caricio <rafael@caricio.com>
2020-06-12 13:23:16 +0200

    Use lvgl-codegen

--- lvgl/build.rs
+++ lvgl/build.rs
@@ -1,48 +1,26 @@
 use std::ffi::OsStr;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::process::Command;
 use std::{env, fs, path};
 
 fn main() {
-    let project_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap())
-        .canonicalize()
-        .unwrap();
+    let manifest_dir = Path::new(env!("CARGO_MANIFEST_DIR"));
+    let widgets_rs_path = manifest_dir.join("src/widgets/generated.rs");
 
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    let gen_code = out_path.join("bindings.rs");
-
-    let code = invoke_command("../target/debug/lvgl-codegen", &[""], &project_dir);
-    fs::write(&gen_code, code).unwrap();
-
-    // // Format generated code
-    // let _ = invoke_command(
-    //     "cargo",
-    //     &["fmt", "-p", gen_code.to_str().unwrap()],
-    //     &project_dir,
-    // );
-}
-
-fn invoke_command<C, I, S, D>(command: C, args: I, cur_dir: D) -> String
-where
-    C: AsRef<OsStr>,
-    I: IntoIterator<Item = S>,
-    S: AsRef<OsStr>,
-    D: AsRef<path::Path>,
-{
-    Command::new(command)
-        .current_dir(cur_dir)
-        .args(args)
-        .output()
-        .ok()
-        .and_then(|output| {
-            if output.status.success() {
-                Some(String::from_utf8_lossy(&output.stdout).trim().to_string())
-            } else {
+    if !widgets_rs_path.exists() {
+        println!("Generating `src/widgets/generated.rs`");
+        let status = Command::new(manifest_dir.join("../target/debug/lvgl-codegen"))
+            .spawn()
+            .unwrap_or_else(|_| {
                 panic!(
-                    "{}",
-                    String::from_utf8_lossy(&output.stderr).trim().to_string()
-                );
-            }
-        })
-        .unwrap()
+                    "Code generation failed because no codegen executable was found. \
+                     Please run `cargo build --package lvgl-codegen` and then try again.",
+                )
+            })
+            .wait()
+            .unwrap();
+        if !status.success() {
+            panic!("Code generation failed");
+        }
+    }
 }
