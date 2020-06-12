ca14ce3c236f094e0186fa0139aefe81f56b552e
Rafael Caricio <rafael@caricio.com>
2020-06-12 12:40:37 +0200

    Add missing C func to link

--- lvgl-sys/string.c
+++ lvgl-sys/string.c
@@ -66,3 +66,18 @@ char *strcpy(char *dest, const char *src)
         /* nothing */;
     return tmp;
 }
+
+int strcmp(const char *cs, const char *ct)
+{
+    unsigned char c1, c2;
+
+    while (1) {
+        c1 = *cs++;
+        c2 = *ct++;
+        if (c1 != c2)
+            return c1 < c2 ? -1 : 1;
+        if (!c1)
+            break;
+    }
+    return 0;
+}
