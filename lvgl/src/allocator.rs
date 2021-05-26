b7bc4e6e01248b9ffdd9ca5aac7eccd9302e3f55
Rafael Caricio <rafael@caricio.com>
2021-05-26 15:21:28 +0200

    Remove lazy_static dependency

--- lvgl/src/allocator.rs
+++ lvgl/src/allocator.rs
@@ -9,12 +9,12 @@ pub struct LvglAlloc;
 unsafe impl GlobalAlloc for LvglAlloc {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
         // Make sure LVGL is initialized!
-        let _ = *crate::LVGL_INITIALIZED;
+        crate::lvgl_init();
         lvgl_sys::lv_mem_alloc(layout.size() as lvgl_sys::size_t) as *mut u8
     }
 
     unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
-        let _ = *crate::LVGL_INITIALIZED;
+        crate::lvgl_init();
         lvgl_sys::lv_mem_free(ptr as *const cty::c_void)
     }
 }
