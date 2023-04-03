9885e0876cf723283a874a18b01a00145b6b8032
Nia Espera <a5b6@riseup.net>
2023-04-03 19:50:42 +0200

    uncomment

--- examples/meter.rs
+++ examples/meter.rs
@@ -1,6 +1,4 @@
-// TODO: Redo this example.
 
-/*
 use embedded_graphics::pixelcolor::Rgb565;
 use embedded_graphics::prelude::*;
 use embedded_graphics_simulator::{
@@ -10,9 +8,10 @@ use lvgl;
 use lvgl::style::{Opacity, Style};
 use lvgl::widgets::Meter;
 use lvgl::{
-    Align, Color, Display, DrawBuffer, LvError, Part, State, Widget,
+    Align, Color, Display, DrawBuffer, LvError, Part, Widget,
 };
 use std::time::Duration;
+use std::time::Instant;
 
 fn main() -> Result<(), LvError> {
     const HOR_RES: u32 = 240;
@@ -38,34 +37,34 @@ fn main() -> Result<(), LvError> {
     screen_style.set_bg_color(Color::from_rgb((0, 0, 0)));
     screen.add_style(Part::Main, &mut screen_style)?;
 
-    // Create the gauge
-    let mut gauge_style = Style::default();
+    // Create the meter
+    let mut meter_style = Style::default();
     // Set a background color and a radius
-    gauge_style.set_radius(5);
-    gauge_style.set_bg_opa(Opacity::OPA_COVER);
-    gauge_style.set_bg_color(Color::from_rgb((192, 192, 192)));
+    meter_style.set_radius(5);
+    meter_style.set_bg_opa(Opacity::OPA_COVER);
+    meter_style.set_bg_color(Color::from_rgb((192, 192, 192)));
     // Set some padding's
-    //gauge_style.set_pad_inner(20);
-    gauge_style.set_pad_top(20);
-    gauge_style.set_pad_left(5);
-    gauge_style.set_pad_right(5);
+    //meter_style.set_pad_inner(20);
+    meter_style.set_pad_top(20);
+    meter_style.set_pad_left(5);
+    meter_style.set_pad_right(5);
 
-    //gauge_style.set_scale_end_color(Color::from_rgb((255, 0, 0)));
-    gauge_style.set_line_color(Color::from_rgb((255, 255, 255)));
-    //gauge_style.set_scale_grad_color(Color::from_rgb((0, 0, 255)));
-    gauge_style.set_line_width(2);
-    //gauge_style.set_scale_end_line_width(4);
-    //gauge_style.set_scale_end_border_width(4);
+    //meter_style.set_scale_end_color(Color::from_rgb((255, 0, 0)));
+    meter_style.set_line_color(Color::from_rgb((255, 255, 255)));
+    //meter_style.set_scale_grad_color(Color::from_rgb((0, 0, 255)));
+    meter_style.set_line_width(2);
+    //meter_style.set_scale_end_line_width(4);
+    //meter_style.set_scale_end_border_width(4);
 
-    let mut gauge = Gauge::create(&mut screen, None)?;
-    gauge.add_style(Part::Main, &mut gauge_style)?;
-    gauge.set_align(&mut screen, Align::Center, 0, 0)?;
-    gauge.set_value(0, 50)?;
+    let mut meter = Meter::create(&mut screen)?;
+    meter.add_style(Part::Main, &mut meter_style)?;
+    meter.set_align(Align::Center, 0, 0)?;
+    meter.set_value(0, 50)?;
 
     let mut i = 0;
     let mut loop_started = Instant::now();
     'running: loop {
-        gauge.set_value(0, i)?;
+        meter.set_value(0, i)?;
 
         lvgl::task_handler();
         window.update(&sim_display);
@@ -94,8 +93,8 @@ fn main() -> Result<(), LvError> {
 
     Ok(())
 }
-*/
 
-fn main() {
-    println!("Currently broken :c")
-}
+
+//fn main() {
+//    println!("Currently broken :c")
+//}
