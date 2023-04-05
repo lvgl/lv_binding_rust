10eb02ffb208dc334cb08535680c98e02c482cfb
Nia <a5b6@riseup.net>
2023-04-05 22:36:56 +0200

    Support for keyboard textarea setting (#96)
    * add simple kb impl

---
+++ lvgl/src/widgets/keyboard.rs
@@ -0,0 +1,16 @@
+use crate::LvResult;
+use crate::widgets::{Keyboard, Textarea};
+use crate::NativeObject;
+
+impl Keyboard {
+    /// Associates a given `Textarea` to the keyboard.
+    pub fn set_textarea(&mut self, textarea: &mut Textarea) -> LvResult<()> {
+        unsafe {
+            lvgl_sys::lv_keyboard_set_textarea(
+                self.raw()?.as_mut() as *mut lvgl_sys::lv_obj_t,
+                textarea.raw()?.as_mut() as *mut lvgl_sys::lv_obj_t,
+            )
+        }
+        Ok(())
+    }
+}
