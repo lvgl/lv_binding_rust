76c03e7471a37f1cc019b904630b740392b8e716
Nia Espera <a5b6@riseup.net>
2023-05-09 16:04:36 +0200

    add slider methods

---
+++ lvgl/src/widgets/slider.rs
@@ -0,0 +1,20 @@
+use crate::widgets::Slider;
+use crate::{LvResult, Animation};
+use crate::lv_core::obj::NativeObject;
+
+impl Slider {
+    /// Set a new value on the slider
+    pub fn set_value(&self, value: i32, anim: Animation) -> LvResult<()> {
+        unsafe {
+            lvgl_sys::lv_bar_set_value(self.core.raw()?.as_ptr(), value, anim.into())
+        }
+        Ok(())
+    }
+
+    /// Gets the current value of the slider
+    pub fn get_value(&self) -> LvResult<i32> {
+        unsafe {
+            Ok(lvgl_sys::lv_bar_get_value(self.core.raw()?.as_ptr()))
+        }
+    }
+}
